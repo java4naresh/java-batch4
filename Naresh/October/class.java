fe83c9dd090d720b3b8c3ba9072ed03c230951bc
java4naresh <kambalanaresh4java@gmail.com>
2022-10-21 08:23:10 +0530

    21-10-2022

---
+++ Naresh/October/class.java
@@ -0,0 +1,10 @@
+class PublicClass {
+public static void main(String[] args) {
+System.out.println("My Name Is Naresh");
+System.out.println(10+20);
+System.out.println(10*20);
+System.out.println(10-20);
+System.out.println(20%10);
+System.out.println(20/10);
+}
+}
