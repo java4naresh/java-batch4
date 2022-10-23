3858dfd359cbfc5de65e39b3ab8ef4e880459244
Dhanalakshmimannem <115989660+Dhanalakshmimannem@users.noreply.github.com>
2022-10-23 08:49:02 +0530

    class

--- Dhanalakshmi/practice file/class.java
+++ Dhanalakshmi/practice file/class.java
@@ -27,3 +27,20 @@ System.out.println(20/10);
 }
 
 }
+
+
+
+class PubilcClass3  {
+
+public static void main(String[]args){
+System.out.println("This class is PubilcClass2");
+System.out.println("My Name Is Dhanalakshmi");
+System.out.println(10+20);
+System.out.println(10*20);
+System.out.println(10-20);
+System.out.println(10%20);
+System.out.println(20%10);
+System.out.println(20/10);
+}
+
+}
