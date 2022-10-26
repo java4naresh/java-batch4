517d3342101ce3a8d4b42f67e1d1f36e70d91ea8
Dhanalakshmimannem <115989660+Dhanalakshmimannem@users.noreply.github.com>
2022-10-26 19:18:26 +0530

    Conditional Operators

--- Dhanalakshmi/practice file/OperatorsExample.java
+++ Dhanalakshmi/practice file/OperatorsExample.java
@@ -24,8 +24,8 @@ class OperatorsExample {
   //System.out.println(++num1 + num1++);//22
   //System.out.println(num1);//12
 
-  //System.out.println(++num1 + num1++ + ++num1);//35 11+11+13
-  //System.out.println(num1);//13
+   System.out.println(++num1 + num1++ + ++num1);//35 11+11+13
+   System.out.println(num1);//13
 
   //System.out.println(++num1 + num1++ + ++num1 + num1++);//48 11+11+13+13
   //System.out.println(num1);//14
@@ -36,8 +36,27 @@ class OperatorsExample {
    //System.out.println(++num2 + num2++ + ++num2);//65 21+21+23
    //System.out.println(num2);//23
 
-   System.out.println(++num2 + num2++ + ++num2 + num2++);//88 21+21+23+23
-   System.out.println(num2);//24
+   //System.out.println(++num2 + num2++ + ++num2 + num2++);//88 21+21+23+23
+   //System.out.println(num2);//24
+
+    //post decrement
+    
+     //System.out.println(num2--);//20
+
+     //System.out.println(num2);//19
+
+                    // 14 +    14  +     16  +   15 +     15
+    System.out.println(++num1 + num1++ + ++num1 + --num1 + num1--);//74
+    System.out.println(num1);//14
+                      // 15  +   15  +   17  +     16  +   16   -   14 -     14    +   19
+    System.out.println(++num1 + num1++ + ++num1 + --num1 + num1-- - --num1 - num1++ + --num2);//70
+    System.out.println(num1);//15
+    System.out.println(num2);//19
+                     // 16 +    16  +    18     + 17   +  17  -    15 -     15    +       20    
+    System.out.println(++num1 + num1++ + ++num1 + --num1 + num1-- - --num1 - num1++ + --num2 + --num2);//89
+    System.out.println(num1);//16
+    System.out.println(num2);//17
+
 
 
   }
---
+++ Dhanalakshmi/practice file/OperatorsExample2.java
@@ -0,0 +1,29 @@
+class OperatorsExample2 {
+
+  public static void main(String[] args){
+
+    int a = 10;
+    int b = 10;
+
+    //Arithmetic Operators
+     int sum = a + b;//20
+     int substraction = a - b;//0
+     int mul = a * b;//100
+     int coef = a / b;//1
+     int rem = a % b;//0
+    System.out.println("sum ="+sum);//20
+    System.out.println("substraction ="+substraction);//0
+    System.out.println("mul ="+mul);//100
+    System.out.println("coef ="+coef);//1
+    System.out.println("rem ="+rem);//0
+
+    System.out.println((int)'d');//100
+
+    System.out.println('d' + 'd');//200
+
+
+
+}
+
+
+}
