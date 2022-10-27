84c1bb368a594aca6c24a391487d65510b1af266
krishnavasavi <krishnavasavi72@gmail.com>
2022-10-27 07:26:47 +0530

    Arthimetic,conditional

---
+++ vasavi/practice File/ConditionalOperators.java
@@ -0,0 +1,44 @@
+class ConditionalOperators {
+
+public static void main(String[] args){
+
+
+ int day = 7;
+
+ if(day == 1){
+    System.out.println("Sunday");
+
+     }
+
+ if(day == 2){
+    System.out.println("Monday");
+
+    }
+
+ if(day == 3){
+    System.out.println("Tuesday");
+
+   }
+
+ if(day == 4){
+    System.out.println("Wednesday");
+
+   }
+
+ if(day == 5){
+    System.out.println("Thrusday");
+
+  }
+
+ if(day == 6){
+    System.out.println("Friday");
+
+   }
+
+
+ if(day == 7){
+    System.out.println("Saturday");
+
+   }
+}
+}
