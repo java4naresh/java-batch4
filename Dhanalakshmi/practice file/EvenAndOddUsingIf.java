e528fa0960a38550af00fb2e1a0cef9b30840f1a
Dhanalakshmimannem <115989660+Dhanalakshmimannem@users.noreply.github.com>
2022-10-28 17:35:55 +0530

    EvenAndOddUsingIf

---
+++ Dhanalakshmi/practice file/EvenAndOddUsingIf.java
@@ -0,0 +1,50 @@
+class EvenAndOddUsingIf {
+ 
+ public static void main(String[] args){
+
+   int num = 7;
+
+   if(num % 2 == 0){
+   System.out.println(num + " Is Even");
+
+}
+
+
+   if(num % 2 != 0){
+   System.out.println(num + " Is Odd");
+
+}
+
+  // find a given character is ovel or not a,e,i,o,u
+
+    char ch = 'c';
+    
+   if(ch == 'a'){
+   System.out.println(ch + "Is Ovel");
+
+}
+
+
+  if(ch == 'e'){
+   System.out.println(ch + "Is Ovel");
+
+}
+  if(ch == 'i'){
+   System.out.println(ch + "Is Ovel");
+
+}
+  if(ch == 'o'){
+   System.out.println(ch + "Is Ovel");
+
+}
+  if(ch == 'u'){
+   System.out.println(ch + "Is Ovel");
+
+}
+
+
+}
+
+
+
+}
