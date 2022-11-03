d92557a84a2e988fb69fc98f030c730fcc7ac015
java4naresh <kambalanaresh4java@gmail.com>
2022-11-03 08:21:34 +0530

    3-11-2022

---
+++ Naresh/November/NestedForLoop.java
@@ -0,0 +1,37 @@
+class NestedForLoop 
+{
+	public static void main(String[] args) 
+	{
+		/*for(int i=1; i<=5; i++) {
+			for(int j=1;j<=5;j++) { 
+				System.out.print(i);//11111
+			}                       //22222
+			System.out.println();   //33333
+		
+		}*/
+
+		/*for(int i=1; i<=5; i++) {
+			for(int j=1;j<=5;j++) { 
+				System.out.print(j);//12345
+			}                       //12345
+			System.out.println();   //12345
+		
+		}*/
+        /*for(int i=6; i<=10; i++) {
+			for(int j=5;j>=1;j--) { //0>=1
+				System.out.print(j);//54321
+			}                       //54321
+			System.out.println();   //54321
+		
+		}*/
+
+        for(int i=5; i>=1; i--) {
+			for(int j=5;j>=1;j--) { //0>=1
+				System.out.print(i);//54321
+			}                       //54321
+			System.out.println();   //54321
+		
+		}
+
+	}
+}
