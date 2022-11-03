aeeb668cbc2076aaf7c33a339c6b36ec8fc854ca
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-03 23:23:54 +0530

    3-11-22

---
+++ vasavi/practice File/NestedForLoop.java
@@ -0,0 +1,15 @@
+class NestedForLoop
+{
+public static void main(String[] args) 
+{
+  for(int i=1; i<=25; i++) 
+  {
+ 
+   for(int j=1;j<=10;j++) 
+   {
+    System.out.println(i+"*"+j+"="+i*j);
+   }
+   System.out.println();
+  }
+}
+}
