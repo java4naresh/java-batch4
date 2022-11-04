15e494dc7c1a1ec5cd6712f6626723ca95ef55e5
Dhanalakshmimannem <115989660+Dhanalakshmimannem@users.noreply.github.com>
2022-11-04 11:30:14 +0530

    NestedForLoopExample

--- Dhanalakshmi/practice file/LeapYearExa.java
+++ Dhanalakshmi/practice file/LeapYearExa.java
@@ -1,16 +1,12 @@
 class LeapYearExa {
+
  public static void main(String[] args){
-
-for(int i=1;i<= 2022; i = i+2){
-System.out.println("Leapyear="+i);
-
-}
-
-
-
-
-}
-
-
-
+   for(int i=1; i <= 2022; i++){ 
+   
+    if(i%4==0) 
+    {
+      System.out.println("Leap year is="+i);
+    } 
+   }
+ } 
 }
---
+++ Dhanalakshmi/practice file/NestedForLoop.java
@@ -0,0 +1,39 @@
+class NestedForLoop {
+
+   public static void main(String[] args)
+   {
+     /*for(int i=1; i<=5; i++){ 
+        for(int j=1;j<=5;j++){ //11111
+         System.out.print(i);  //22222
+         }
+       System.out.println();
+       }*/
+
+     /*for(int i=1; i<=5; i++){
+        for(int j=1;j<=5;j++){
+         System.out.print(j);//12345
+         }                   //12345
+       System.out.println();
+       }*/
+
+      /*for(int i=6;i<=10;i++){
+         for(int j=5;j>=1;j--){
+           System.out.print(j);//54321
+           }
+       System.out.println();
+
+       }*/
+
+       for(int i=5;i>=1;i--){
+          for(int j=5;j>=1;j--){
+           System.out.print(i );//55555
+           }                    //44444
+       System.out.println();
+
+      }
+
+
+
+    }
+} 
+
