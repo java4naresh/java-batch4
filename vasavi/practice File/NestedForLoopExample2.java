229a2cb13d7984891c7949bd1f874f190442fbeb
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-06 08:53:21 +0530

    Nested ForLoop Examples

---
+++ vasavi/practice File/NestedForLoopExample2.java
@@ -0,0 +1,17 @@
+class NestedForLoopExample2 
+{
+	public static void main(String[] args) 
+	{
+          
+             for(int i=5; i>=1; i--) {
+		for(int j=i; j<=5; j++) {
+			
+		     System.out.print(" ");
+		}
+                 for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+                 }
+             System.out.println(" ");
+           }
+        }
+}
