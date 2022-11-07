fe87a0f56dfcb5b550f85c3b808c52e1f29d6d1b
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-07 23:30:51 +0530

    factorial example

---
+++ vasavi/practice File/Factorial.java
@@ -0,0 +1,48 @@
+class Factorial
+{
+  public static void main(String[] args)
+  {
+  
+       /*int num = 4;
+       int fact = 1, i;
+       for(i=2; i<=num; i++) { //1*2*3*4 = 24
+       
+         fact = fact * i; 
+       }
+       System.out.println(fact);*/
+
+       /*int num = 3;
+       int fact = 1, i;
+       for(i=2; i<=num; i++) { //1*2*3 =6
+       
+         fact = fact * i; 
+       }
+       System.out.println(fact);*/
+
+       /*int num = 5;
+       int fact = 1,i;
+       for(i=2; i<=num; i++) { //1*2*3*4*5=120
+       
+         fact = fact * i; 
+       }
+       System.out.println(fact);*/
+    
+       int num = 10;
+       int fact = 1,i;
+       for(i=2; i<=num; i++) { //1*2*3*4*5*6*7*8*9*10 = 3628800
+       
+         fact = fact * i; 
+       }
+       System.out.println(fact);
+     
+       /*int num = 20;
+       int fact = 1,i;
+       for(i=2; i<=num; i++) { //1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20=2102132736
+       
+         fact = fact * i; 
+       }
+       System.out.println(fact);*/
+    
+  }
+
+}
--- vasavi/practice File/WhileLoopExample.java
+++ vasavi/practice File/WhileLoopExample.java
@@ -2,21 +2,23 @@ class WhileLoopExample
 {
 	public static void main(String[] args) 
 	{
-             int num,rem;
-             int rev=0;
-             int temp;
+             
 		for(int i=1;i<=1000;i++)
                 {
-                  num=i;
-		 while(num>0) 
+                   int num,rem;
+                    num=i;
+                    int rev=0;
+                    int temp;
+                    temp=num;
+		 while(num!=0) 
                 {
-                  temp= num;
+                 
                   rem = num % 10;
                   num = num / 10;
 		  rev = (rev * 10) + rem;
 		}
-		if(rev == num) 
-		System.out.println(num +"");
+		if(rev == temp) 
+		System.out.println(temp +"");
                               
               }
 	}
