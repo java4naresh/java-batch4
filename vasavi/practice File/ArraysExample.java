598aba40e54d907b8d012de454ef65821d66401f
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-08 22:07:35 +0530

    Arrays Example

---
+++ vasavi/practice File/ArraysExample.java
@@ -0,0 +1,57 @@
+class ArraysExample 
+{
+	public static void main(String[] args) 
+	{
+		// we can create array 2 ways
+		// first way
+		
+                /*String[] a = {"Apple","Banana","Mango","Grapes"};
+		System.out.println(a[0]);//Apple
+		System.out.println(a[1]);//Banana
+		System.out.println(a[2]);//Mango
+		System.out.println(a[3]);//Grapes*/
+		
+		//a[0] ="Abcd" ;
+		/*System.out.println(a[0]);//Abcd
+		System.out.println(a[1]);//Banana
+		System.out.println(a[2]);//Mango
+		System.out.println(a[3]);//Grapes*/
+               
+                /*for(int i=0; i < a.length; i++) {
+		    System.out.println(a[i]);
+                  }*/
+
+              /*char a[] = {'a','b','c','d'};
+		System.out.println(a[0]);//a
+		System.out.println(a[1]);//b
+		System.out.println(a[2]);//c
+		System.out.println(a[3]);//d*/
+               
+               /*a[0] ='z' ;
+		System.out.println(a[0]);//z
+		System.out.println(a[1]);//b
+		System.out.println(a[2]);//c
+		System.out.println(a[3]);//d*/
+               
+                /*for(int i=0; i < a.length; i++) {
+		    System.out.println(a[i]);
+                 }*/
+
+                float a[] = {55.4f,55.2f,67.3f,85.0f};
+		System.out.println(a[0]);//55.40
+		System.out.println(a[1]);//55.28
+		System.out.println(a[2]);//36.85
+		System.out.println(a[3]);//85.00
+               
+                a[0] =25.4f ;
+		System.out.println(a[0]);//25.40
+		System.out.println(a[1]);//55.28
+		System.out.println(a[2]);//36.85
+		System.out.println(a[3]);//85.00
+                /*for(int i=0; i < a.length; i++) {
+		    System.out.println(a[i]);
+                 }*/
+             
+				
+	}
+}
