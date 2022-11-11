2d3ef7fffe2bd845b7a4832d5f6305aea4fc0195
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-12 00:42:41 +0530

    2-Dimensional example

---
+++ vasavi/practice File/Dimensional.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Dimensional
+{
+	public static void main(String[] args) 
+	{
+		int [][] numbers = {{1,2,3},{4,5,6},{7,8,9}};
+		System.out.println(numbers[1][1]);
+		System.out.println(numbers[2][0]);
+		System.out.println(numbers[2][2]);
+		System.out.println(Arrays.toString(numbers[0]));
+		System.out.println(Arrays.toString(numbers[1]));
+		System.out.println(Arrays.toString(numbers[2]));
+
+		for(int i=0; i< numbers.length; i++) {
+		for(int j=0; j < numbers[i].length; j++) {//numbers[0]
+		if(j == numbers[i].length - 1) {
+		int temp = numbers[0][i];
+                numbers[0][i] = numbers[j][i];
+		numbers[j][i] = temp;
+		}
+		}
+		}
+              System.out.println("============================");
+		System.out.println(Arrays.toString(numbers[0]));
+		System.out.println(Arrays.toString(numbers[1]));
+		System.out.println(Arrays.toString(numbers[2]));
+	}
+}
