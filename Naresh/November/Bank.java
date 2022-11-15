e24ba8845c0c6906754d01e31e70ff84a7ad05f5
java4naresh <kambalanaresh4java@gmail.com>
2022-11-15 08:27:54 +0530

    15-11-2022 class

--- Naresh/November/Bank.java
+++ Naresh/November/Bank.java
@@ -5,10 +5,25 @@ class Bank
 	int branchId;
 	String managerName;
 
+    public Bank() { // default constructor or no param constructor
+     System.out.println("no param constructor called");
+    }
+
+	public Bank(String name, String brch, int id, String mngrName) {//Param Constructor
+	bankName = name;
+	branch = brch;
+	branchId = id;
+    managerName = mngrName;
+	}
+
+	public Bank(String name, String brch, int id, String mngrName, String k) {
+	
+	}
+
 
 	public static void main(String[] args) 
 	{
-		Bank bank = new Bank();
+		Bank bank = new Bank();// no param constructor calling
 		System.out.println(bank.bankName+" "+ bank.branch+" "+bank.branchId+" "+bank.managerName);
 		bank.bankName = "HDFC";
         bank.branch = "KPHB";
@@ -22,5 +37,7 @@ class Bank
         bank2.branchId = 250;
         bank2.managerName = "Naresh";
 		System.out.println(bank2.bankName+" "+ bank2.branch+" "+bank2.branchId+" "+bank2.managerName);
+		Bank bank3 = new Bank("SBI", "KPHB", 125, "Naresh");// param constructor calling
+        System.out.println(bank3.bankName+" "+ bank3.branch+" "+bank3.branchId+" "+bank3.managerName);
 	}
 }
