491ddb41d2c459f0b019141598e0c46263ea5686
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-15 19:35:05 +0530

    constructors

--- vasavi/practice File/IndianStatesExample.java
+++ vasavi/practice File/IndianStatesExample.java
@@ -1,13 +1,38 @@
-class IndianStates{
+class IndianStatesExample{
  
  String stateName;
  String countryName;
  String chiefMinister;
  long totalPopulation;
- 
-  public static void main(String[] args){
+  public IndianStatesExample(){// default constructor or no param constructor
+     System.out.println("no param constructor called");
+ }
+  public IndianStatesExample(String state,String country,String CM,long population)//parameterized constructor
+  {
+    stateName= state;
+    countryName=country;
+    chiefMinister= CM;
+    totalPopulation=population;
+  } 
+  public IndianStatesExample(String state,String country,String CM,long population,String I)
+  {
+    
 
-  System.out.println( );
+  }
+ 
+   public static void main(String[] args){
+   IndianStatesExample state=new IndianStatesExample();
+     state.stateName="Telagana";
+    state.countryName=" India";
+    state.chiefMinister= " kcr";
+    state.totalPopulation= 10000234;
+   System.out.println(state.stateName+" "+ state.countryName+" "+state.chiefMinister+" "+state.totalPopulation);
+    state.stateName="Andhra Pradesh";
+    state.countryName=" India";
+    state.chiefMinister= " jagan";
+    state.totalPopulation= 1000023;
+   
+  System.out.println(state.stateName+" "+ state.countryName+" "+state.chiefMinister+" "+state.totalPopulation );
 
   }
 
