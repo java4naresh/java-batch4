ce66681a4692a583929fce4fab2da08bb9913ae8
Dhanalakshmimannem <115989660+Dhanalakshmimannem@users.noreply.github.com>
2022-11-15 21:28:47 +0530

    15-11-2022

--- Dhanalakshmi/practice file/College.java
+++ Dhanalakshmi/practice file/College.java
@@ -7,9 +7,32 @@ class College
   int totalstaff;
 
 
+  public College() { // default constructor or no param constructor
+   System.out.println("no param constructor");
+  
+  }
+
+  public College(String name, String campName,int students, String princiName, int staff) { //param constructor
+  collegeName = name;
+  campusName = campName;
+  studentstotal = students;
+  principalName = princiName;
+  totalstaff = staff;
+  }
+ 
+  public College(String name, String campName,int students, String princiName) {
+  collegeName = name;
+  campusName = campName;
+  studentstotal = students;
+  principalName = princiName;
+  
+  }
+   
+
+
   public static void main(String[] args)
   {
-     College college = new College();
+     College college = new College();//no param constructor
      System.out.println(college.collegeName+" "+ college.campusName+" "+ college.studentstotal+" "+college.principalName+" "+college.totalstaff); 
      college.collegeName = "ANU";
      college.campusName = "saraswathi";
@@ -26,6 +49,12 @@ class College
      college2.principalName = "Dhana";
      college2.totalstaff = 30;
      System.out.println(college2.collegeName+" "+ college2.campusName+" "+ college2.studentstotal+" "+college2.principalName+" "+college2.totalstaff); 
+     
+     College college3 = new College("Narayana","boys",2000,"Charanya", 150);
+     System.out.println(college3.collegeName+" "+ college3.campusName+" "+ college3.studentstotal+" "+college3.principalName+" "+college3.totalstaff); 
+
+     College college4 = new College("Chalapathi","boys & girls",5000,"Dhanalakshmi");//param constructor
+     System.out.println(college4.collegeName+" "+ college4.campusName+" "+ college4.studentstotal+" "+college4.principalName); 
 
 
   }
--- Dhanalakshmi/practice file/Office.java
+++ Dhanalakshmi/practice file/Office.java
@@ -2,23 +2,48 @@ class Office
 { 
   String officeName;
   String employeesName;
-  int employeestotal;
   int employeesid;
   int totalstaff;
   String managerName;
+
+  public Office() { // default constructor or no param constructor
+  System.out.println("no param constructor");
+ 
+  }
+
+  public Office(String name, String employName,int id, int staff, String mngrName) { //param constructor
+  officeName = name;
+  employeesName = employName;
+  employeesid = id;
+  totalstaff = staff;
+  managerName = mngrName;
+  }
+  
+  public Office(String name, String employName,int id,String mngrName){
+  officeName = name;
+  employeesName = employName;
+  employeesid = id;
+  managerName = mngrName;
+  
+  }
+
+
   public static void main(String[] args)
   {
-     Office office = new Office();
-     System.out.println(office.officeName+" "+office.employeesName+" "+ office.employeestotal+" "+office.employeesid+" "+office.totalstaff+" "+office.managerName); 
+     Office office = new Office();//no param constructor
+     System.out.println(office.officeName+" "+office.employeesName+" "+office.employeesid+" "+office.totalstaff+" "+office.managerName); 
      office.officeName = "TCS";
      office.employeesName = "Dhanu";
-     office.employeestotal =  1000;
      office.employeesid = 1234;
      office.totalstaff = 500;
      office.managerName = "Dhanu";
-     System.out.println(office.officeName+" "+office.employeesName+" "+ office.employeestotal+" "+office.employeesid+" "+office.totalstaff+" "+office.managerName); 
+     System.out.println(office.officeName+" "+office.employeesName+" "+office.employeesid+" "+office.totalstaff+" "+office.managerName); 
+          
+     Office office2 = new Office("TECHMAHINDRA","Dhanu",123,1000,"Charanya");//param constructor
+     System.out.println(office2.officeName+" "+office2.employeesName+" "+office2.employeesid+" "+office2.totalstaff+" "+office2.managerName);
 
-    
+     Office office3 = new Office("Wipro","charanya",1234,"Varsha");//param constructor
+     System.out.println(office3.officeName+" "+office3.employeesName+" "+office3.employeesid+" "+office3.managerName);
 
   }
 
