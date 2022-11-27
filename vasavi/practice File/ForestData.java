de7dcc5cb5ecfa5c765816e6f172f336af2ec876
krishnavasavi <krishnavasavi72@gmail.com>
2022-11-27 09:35:16 +0530

    27-11-22

---
+++ vasavi/practice File/ForestData.java
@@ -0,0 +1,10 @@
+class ForestData
+{
+	public static void main(String[] args) 
+	{
+		Forest me = new Forest();
+                System.out.println("Amphibians: "+ me.getAmphibians());
+                System.out.println("Mammals: "+ me.getMammals());
+		System.out.println("Fish: "+ me.getFish());
+	}
+}
---
+++ vasavi/practice File/Inheritance.java
@@ -0,0 +1,26 @@
+class Animals
+{
+	public String getAmphibians() {
+		return "frogs";
+	}
+
+	public String getbirds() {
+		return "parrots";
+	}
+	
+}
+
+class Forest extends Animals
+{
+	public String getFish() {
+		return "hagfish";
+	}
+
+	public String getMammals() {
+		return "elephants";
+	}
+
+	public String getReptiles() {
+		return "tortoises";
+	}
+}
