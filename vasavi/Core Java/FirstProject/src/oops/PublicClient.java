61fb6202859b49924a5ed8a38f61c62dcb12bc94
krishnavasavi <krishnavasavi72@gmail.com>
2022-12-07 00:02:36 +0530

    6-12-22

---
+++ vasavi/Core Java/FirstProject/src/oops/PublicClient.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class PublicClient extends PublicAccessModifier {
+
+	public static void main(String[] args) {
+		PublicClient pa = new PublicClient();
+        //System.out.println(pa.name);
+		System.out.println(pa.name);
+	}
+
+}
