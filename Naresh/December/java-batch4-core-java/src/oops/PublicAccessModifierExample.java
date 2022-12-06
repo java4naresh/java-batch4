89bb137f6685aab05e58ac26dca3a893c8110db9
java4naresh <kambalanaresh4java@gmail.com>
2022-12-06 08:46:58 +0530

    6-12-2022

---
+++ Naresh/December/java-batch4-core-java/src/oops/PublicAccessModifierExample.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class PublicAccessModifierExample {
+	
+  private String name1;
+  String name2;
+  protected String name3;
+  public String name4;
+
+}
---
+++ Naresh/December/java-batch4-core-java/src/test/PublicClient.java
@@ -0,0 +1,16 @@
+package test;
+
+import oops.PublicAccessModifierExample;
+
+public class PublicClient extends PublicAccessModifierExample {
+
+	public static void main(String[] args) {
+		PublicClient pame = new PublicClient();
+        //System.out.println(pame.name);
+		System.out.println(pame.name1);
+		System.out.println(pame.name2);
+		System.out.println(pame.name3);
+		System.out.println(pame.name4);
+	}
+
+}
