9bc1ca6ccfd16cdfe3ad6e01a04b7b31ccdfb362
krishnavasavi <krishnavasavi72@gmail.com>
2022-12-26 07:48:04 +0530

    26-12-22

---
+++ vasavi/corejava/FirstProject/src/exception/EceptionHandlingExampl4.java
@@ -0,0 +1,38 @@
+package exception;
+
+public class EceptionHandlingExampl4 {
+
+		public static void main(String[] args) {
+			    try{
+			    
+				String name = null;
+				name.charAt(1);
+				} catch(ArithmeticException e) {
+
+				} catch(NullPointerException e) {
+
+				} catch(Exception e) {
+
+				} finally {
+				System.out.println("Cleanup");
+				}
+			    
+			
+			System.out.println(getDivisionNumber());//1
+		}
+	
+		
+		public static int getDivisionNumber() {
+			try {
+				return 10/0;
+			} catch(ArithmeticException e) {
+				return 0;
+			} finally {
+				
+			}
+		}
+
+	}
+
+
+
