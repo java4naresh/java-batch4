43923349a6c556f361a3a013cf777283ebd375e1
pavan <pavankumarpallapati@!gmail.com>
2023-02-01 13:45:18 +0530

    Merge Folders data into one file

--- Pavan/Practice file/Inheritance/Batch4/src/Test/FilesMergeFolders.java
+++ Pavan/Practice file/Inheritance/Batch4/src/Test/FilesMergeFolders.java
@@ -10,41 +10,40 @@ import java.io.PrintWriter;
 import java.util.Stack;
 
 public class FilesMergeFolders {
-	
-		public static void main(String[] args) throws IOException
-		{
-			File dir = new File("D:\\Task");
-			PrintWriter pw = new PrintWriter("D:\\Output.txt");
-			String[] fileNames = dir.list();
-			for(String fileName : fileNames) {
-				File file = new File("D:\\Task", fileName);
-				if(file.isFile()) {
-					System.out.println("files"+ fileName);
-					BufferedReader br = new BufferedReader(new FileReader("D:\\Task\\" + fileName));
-					String line = br.readLine();
-					while(line != null) {
-						pw.println(line);
-						line = br.readLine();
-					}
-					pw.flush();
-				} else if(file.isDirectory()) {
-					String[] fileNames2 = file.list();
-					for(String fileName2 : fileNames2) {
-						File file1 = new File("D:\\Task\\" + fileName, fileName2);
-						if(file1.isFile()) {
-							System.out.println("files"+ fileName);
-							BufferedReader br = new BufferedReader(new FileReader("D:\\Task\\" + fileName + "\\" + fileName2));
-							String line = br.readLine();
-							while(line != null) {
-								pw.println(line);
-								line = br.readLine();
-							}
-							pw.flush();
-						}
-					}
-					
+
+	public static void main(String[] args) throws IOException {
+		File dir = new File("D:\\Task");
+		PrintWriter pw = new PrintWriter("D:\\Output.txt");
+		String[] fileNames = dir.list();
+		for (String fileName : fileNames) {
+			File file = new File("D:\\Task", fileName);
+			if (file.isFile()) {
+				System.out.println("files" + fileName);
+				BufferedReader br = new BufferedReader(new FileReader("D:\\Task\\" + fileName));
+				String line = br.readLine();
+				while (line != null) {
+					pw.println(line);
+					line = br.readLine();
 				}
+				pw.flush();
+			} else if (file.isDirectory()) {
+				String[] fileNames2 = file.list();
+				for (String fileName2 : fileNames2) {
+					File file1 = new File("D:\\Task\\" + fileName, fileName2);
+					if (file1.isFile()) {
+						System.out.println("files" + fileName);
+						BufferedReader br = new BufferedReader(
+								new FileReader("D:\\Task\\" + fileName + "\\" + fileName2));
+						String line = br.readLine();
+						while (line != null) {
+							pw.println(line);
+							line = br.readLine();
+						}
+						pw.flush();
+					}
+				}
+
 			}
 		}
+	}
 }
-
