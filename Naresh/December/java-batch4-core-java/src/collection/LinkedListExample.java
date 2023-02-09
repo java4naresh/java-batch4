e19110da593f8e38301f57d773f718cc5e898c69
java4naresh <kambalanaresh4java@gmail.com>
2023-02-09 08:43:32 +0530

    LinkedList

---
+++ Naresh/December/java-batch4-core-java/src/collection/LinkedListExample.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		List<Integer> list = new LinkedList<>();
+		list.add(1000);//0
+		list.add(1);//1
+		list.add(4);//2
+		list.add(32);//3
+		list.add(5);//4
+		list.add(2);//5
+		list.add(1);//6
+		System.out.println(list);
+	}
+
+}
