324e5828cc0ed5cd8835a0fc42a522bc3f9f70a0
java4naresh <kambalanaresh4java@gmail.com>
2023-02-17 08:45:55 +0530

    17-02-2023

---
+++ Naresh/December/java-batch4-core-java/src/collection/CitizenAgeSorting.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.Comparator;
+
+public class CitizenAgeSorting implements Comparator<Citizen>{
+
+	@Override
+	public int compare(Citizen o1, Citizen o2) {
+		return o1.getAge() - o2.getAge();
+	}
+
+}
---
+++ Naresh/December/java-batch4-core-java/src/collection/CitizenNameSorting.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.Comparator;
+
+public class CitizenNameSorting implements Comparator<Citizen>{
+
+	@Override
+	public int compare(Citizen o1, Citizen o2) {
+		return o1.getCitizenName().compareTo(o2.getCitizenName());
+	}
+
+}
