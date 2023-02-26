a73275535c25ceb629f7f9fce63eea68619f044e
pavan <pavankumarpallapati@!gmail.com>
2023-02-26 08:47:56 +0530

    Task

--- Pavan/Practice file/Inheritance/Batch4/src/collection/StudentInformation.java
+++ Pavan/Practice file/Inheritance/Batch4/src/collection/StudentInformation.java
@@ -3,46 +3,69 @@ package collection;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 public class StudentInformation {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 
-		Map<String, List<String>> test = new HashMap<>();
+		FileReader fr = new FileReader("E:\\Student.txt");
+		BufferedReader br = new BufferedReader(fr);
+		String marksList;
 
-		FileReader fr = new FileReader("F:\\Student1.txt");
-		try (BufferedReader br = new BufferedReader(fr)) {
-			String i;
+		while ((marksList = br.readLine()) != null) {
+			String arry[] = marksList.split(" ");
+			String studentName = arry[0];
+			// if(studentName=="Pavan")
+			String studentSemester = arry[1];
 
-			while ((i = br.readLine()) != null) {
-				String data[] = i.split(" ");
-				i = i.substring(0, i.length() - 1);
-				List<String> datalist = new ArrayList<String>();
-				datalist.add(data[0]);
-				datalist.add(data[1]);
-				datalist.add(data[2]);
-				datalist.add(data[3]);
-				datalist.add(data[4]);
-				datalist.add(data[5]);
-				datalist.add(data[6]);
-				datalist.add(data[7]);
-				test.put(data[1], datalist);
-				Iterator<String> itr = datalist.iterator();
-				while (itr.hasNext()) {
-					// System.out.println(itr.next());
-					String datalist1 = itr.next();
+			String studentSubjects1[] = arry[2].split("-");
+			String studentSubject1 = studentSubjects1[0];
+			int marks1 = Integer.parseInt(studentSubjects1[1]);
 
-				}
-			}
-			for (String datalist1 : test.keySet()) {
-				System.out.println("key is " + datalist1 + "Data" + test.get(datalist1));
-			}
+			String studentSubjects2[] = arry[3].split("-");
+			String studentSubject2 = studentSubjects2[0];
+			int marks2 = Integer.parseInt(studentSubjects2[1]);
+
+			String studentSubjects3[] = arry[4].split("-");
+			String studentSubject3 = studentSubjects3[0];
+			int marks3 = Integer.parseInt(studentSubjects3[1]);
+
+			String studentSubjects4[] = arry[5].split("-");
+			String studentSubject4 = studentSubjects4[0];
+			int marks4 = Integer.parseInt(studentSubjects4[1]);
+
+			String studentSubjects5[] = arry[6].split("-");
+			String studentSubject5 = studentSubjects5[0];
+			int marks5 = Integer.parseInt(studentSubjects5[1]);
+
+			String studentSubjects6[] = arry[7].split("-");
+			String studentSubject6 = studentSubjects6[0];
+			int marks6 = Integer.parseInt(studentSubjects6[1]);
+
+			Map<String, Integer> subjectMarks = new HashMap<>();
+
+			subjectMarks.put(studentSubject1, marks1);
+			subjectMarks.put(studentSubject2, marks2);
+			subjectMarks.put(studentSubject3, marks3);
+			subjectMarks.put(studentSubject4, marks4);
+			subjectMarks.put(studentSubject5, marks5);
+			subjectMarks.put(studentSubject6, marks6);
+			// System.out.println(subjectMarks);
+
+			Map<String, Map<String, Integer>> semesterMarks = new HashMap<>();
+			semesterMarks.put(studentSemester, subjectMarks);
+			// System.out.println(semistarMarks);
+
+			Map<String, Map<String, Map<String, Integer>>> studentSemesterMarks = new HashMap<>();
+
+			studentSemesterMarks.put(studentName, semesterMarks);
+			System.out.println(studentSemesterMarks);
 
 		}
+
 	}
+
 }
